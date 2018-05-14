budziq/rust-skeptic

commit de55251f82d9259a49adc4aa346e8270ddba9fa2

update cargo_metadata

src/skeptic/lib.rs
@@ -553,7 +553,10 @@ pub mod rt {
         fn from_path<P: AsRef<Path>>(pth: P) -> Result<LockedDeps> {
             let pth = pth.as_ref().join("Cargo.toml");
             let metadata = cargo_metadata::metadata_deps(Some(&pth), true)?;
-            let workspace_members = metadata.workspace_members;
+            let workspace_members:Vec<String> = metadata.workspace_members
+                .into_iter()
+                .map(|workspace| workspace.name.clone())
+                .collect();
             let deps = metadata.resolve.ok_or("Missing dependency metadata")?
                 .nodes
                 .into_iter()

src/skeptic/Cargo.toml
@@ -21,7 +21,7 @@ tempdir = "0.3.5"
 glob = "0.2"
 walkdir = "2.0"
 serde_json = "1.0.3"
-cargo_metadata = "0.3"
+cargo_metadata = "0.5"
 bytecount = "0.3.1"
 
 [dev-dependencies]

commit e2906483b39bfd153e29333dd917520c43c8c998

Fix rust-cookbook regression caused by https://github.com/budziq/rust-skeptic/pull/79

src/skeptic/lib.rs
@@ -554,7 +554,7 @@ pub mod rt {
             let metadata = cargo_metadata::metadata_deps(Some(&pth), true)?;
             let workspace_members:Vec<String> = metadata.workspace_members
                 .into_iter()
-                .map(|workspace| workspace.name.clone())
+                .map(|workspace| {format!("{} {} ({})", workspace.name, workspace.version, workspace.url)})
                 .collect();
             let deps = metadata.resolve.ok_or("Missing dependency metadata")?
                 .nodes